2de28ea847236fdb58f4bb677e2090b8b175cc90
Sourav Chandra Dey <100287945+scd-02@users.noreply.github.com>
2023-09-28 08:03:16 +0530

    Added sum.java
    Signed-off-by: Sourav Chandra Dey <100287945+scd-02@users.noreply.github.com>

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+
+  public static int sumOfNo(int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Sum of nos" + sumOfNo(5, 6));
+  }
+}
